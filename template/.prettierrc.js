41796dfc2192ea62d1cca196a2ef526e0bb34a68
Inhwan Lee <lh1227@yahoo.co.kr>
2022-05-14 02:47:37 +0900

    Initial commit

---
+++ template/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	arrowParens: "always",
+	bracketSameLine: true,
+	bracketSpacing: true,
+	singleQuote: false,
+	trailingComma: "all",
+	tabWidth: 2,
+	useTabs: true,
+	printWidth: 80,
+};
