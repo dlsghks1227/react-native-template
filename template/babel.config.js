79d7b2010036dd1cc41661b9f568e88a6a29c336
Inhwan Lee <lh1227@yahoo.co.kr>
2022-05-17 21:36:45 +0900

    Fix path alias error

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,32 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+	presets: ["module:metro-react-native-babel-preset"],
+	plugins: [
+		[
+			"module-resolver",
+			{
+				root: ["./src"],
+				extensions: [
+					".ios.ts",
+					".android.ts",
+					".ts",
+					".ios.tsx",
+					".android.tsx",
+					".tsx",
+					".jsx",
+					".js",
+					".json",
+				],
+				alias: {
+					"@": "./src",
+					"@components": "./src/components",
+					"@contexts": "./src/contexts",
+					"@services": "./src/services",
+					"@store": "./src/store",
+					"@theme": "./src/theme",
+					"@utils": "./src/utils",
+					"@views": "./src/views",
+				},
+			},
+		],
+	],
 };
